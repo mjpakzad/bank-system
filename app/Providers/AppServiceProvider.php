b790b3a0e8d0aa2b19b4eda6781a16523d307ea9
Mj Pakzad <mj.pakzad@outlook.com>
2024-10-16 03:53:39 +0330

    Bind all Contracts to Concrete classes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,16 @@
 
 namespace App\Providers;
 
+use App\Repositories\AccountRepository;
+use App\Repositories\CardRepository;
+use App\Repositories\Contracts\AccountRepositoryInterface;
+use App\Repositories\Contracts\CardRepositoryInterface;
+use App\Repositories\Contracts\TransactionRepositoryInterface;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\TransactionRepository;
+use App\Repositories\UserRepository;
+use App\Strategies\Contracts\TransactionStrategyInterface;
+use App\Strategies\StandardTransactionStrategy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +29,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(AccountRepositoryInterface::class, AccountRepository::class);
+        $this->app->bind(CardRepositoryInterface::class, CardRepository::class);
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        $this->app->bind(TransactionStrategyInterface::class, StandardTransactionStrategy::class);
     }
 }
